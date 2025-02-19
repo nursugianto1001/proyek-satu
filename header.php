3e89839d73c333a0b5f7dacf8e749171c5e66e8b
nursugianto1001 <h1101231053@student.untan.ac.id>
2025-02-19 15:04:40 +0700

    Create header.php

---
+++ header.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="logo">
+                    <a href="index.php">
+                        <img src="images/logo.png" alt="logo">
+                    </a>
+                </div>
+            </div>
+</header>
